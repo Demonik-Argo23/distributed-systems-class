2fdbe1813db05e866b95536b07ac233b2debc2f9
Demonik-Argo23 <cerdivaca9@gmail.com>
2025-08-24 14:09:57 -0600

    feat: implement initial setup for Pokemon API with CRUD operations and database integration

---
+++ PokemonApi/Infrastructure/Entities/PokemonEntity.cs
@@ -0,0 +1,39 @@
+using PokemonApi.Infrastructure.Entities;
+
+namespace PokemonApi.Infrastructure.Entities
+{
+
+    public class PokemonEntity
+    {
+        //GUID es una buena opción para la clave primaria, ya que es única y no se repite.
+        //Además, permite una mejor escalabilidad en sistemas distribuidos.
+        public Guid Id
+        {
+            get; set;
+        }
+        public string Name
+        {
+            get; set;
+        }
+        public string Type
+        {
+            get; set;
+        }
+        public int Level
+        {
+            get; set;
+        }
+        public string Attack
+        {
+            get; set;
+        }
+        public string Defense
+        {
+            get; set;
+        }
+        public string Speed
+        {
+            get; set;
+        }
+    }
+}
