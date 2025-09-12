8b50afdae2ff6d1102e679ffd763ae6002b11021
Demonik-Argo23 <cerdivaca9@gmail.com>
2025-09-11 21:16:09 -0600

    feat: implement Zelda Weapons API with PostgreSQL integration and weapon management

---
+++ ZeldaApi/src/main/java/com/zelda/weapons/enums/WeaponType.java
@@ -0,0 +1,13 @@
+package com.zelda.weapons.enums;
+
+public enum WeaponType {
+    Bat,
+    Boomerang,
+    Club,
+    Hammer,
+    Rod,
+    Spear,
+    OneHandedSword,
+    TwoHandedSword,
+    Other
+}
