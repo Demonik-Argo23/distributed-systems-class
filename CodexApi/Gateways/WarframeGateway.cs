5ef61b0b98cd1c88bc9156de36085b7c617a5689
Demonik-Argo23 <cerdivaca9@gmail.com>
2025-09-21 19:26:04 -0600

    refactor: WarframeGateway by reusing binding and endpoint fields, deleted null possible response

--- CodexApi/Gateways/WarframeGateway.cs
+++ CodexApi/Gateways/WarframeGateway.cs
@@ -8,26 +8,24 @@ namespace CodexApi.Gateways;
 public class WarframeGateway : IWarframeGateway
 {
     private readonly string _soapServiceUrl;
+    private readonly BasicHttpBinding _binding;
+    private readonly EndpointAddress _endpoint;
 
     public WarframeGateway(IConfiguration configuration)
     {
         _soapServiceUrl = configuration["WarframeApi:BaseUrl"] ?? "http://localhost:8080";
+        _binding = new BasicHttpBinding();
+        _endpoint = new EndpointAddress($"{_soapServiceUrl}/WarframeService.svc");
     }
 
     public async Task<Warframe?> GetWarframeByIdAsync(Guid id, CancellationToken cancellationToken)
     {
         try
         {
-            var binding = new BasicHttpBinding();
-            var endpoint = new EndpointAddress($"{_soapServiceUrl}/WarframeService.svc");
-            
-            using var channelFactory = new ChannelFactory<IWarframeServiceContract>(binding, endpoint);
+            using var channelFactory = new ChannelFactory<IWarframeServiceContract>(_binding, _endpoint);
             var client = channelFactory.CreateChannel();
 
             var warframeDto = await client.GetWarframeById(id, cancellationToken);
-            
-            if (warframeDto == null)
-                return null;
 
             return ToModel(warframeDto);
         }
