9ff1a099a50ef45c648b4c5a50e6a2aed4dfbb10
Demonik-Argo23 <cerdivaca9@gmail.com>
2025-10-27 21:32:05 -0600

    feat: add CreateTrainers and GetAllTrainersByName methods to TrainerService, enhance TrainerMapper and ITrainerRepository for new functionality

--- TrainerApi/Mappers/TrainerMapper.cs
+++ TrainerApi/Mappers/TrainerMapper.cs
@@ -35,9 +35,51 @@ public static class TrainerMapper
             Id = trainer.Id,
             Name = trainer.Name,
             Age = trainer.Age,
-            Birthday = Timestamp.FromDateTime(trainer.Birthdate.ToUniversalTime()),
+            Birthdate = Timestamp.FromDateTime(trainer.Birthdate.ToUniversalTime()),
             CreatedAt = Timestamp.FromDateTime(trainer.CreatedAt.ToUniversalTime()),
             Medals = { trainer.Medals.Select(m => m.ToResponse()) }
+        };
+    }
+
+    public static Trainer ToModel(this CreateTrainerRequest request)
+    {
+        return new Trainer
+        {
+            Name = request.Name,
+            Age = request.Age,
+            Birthdate = request.Birthdate.ToDateTime(),
+            Medals = request.Medals.Select(m => m.ToModel()).ToList()
+        };
+    }
+
+    public static TrainerDocument ToDocument(this Trainer trainer)
+    {
+        return new TrainerDocument
+        {
+            Id = trainer.Id,
+            Name = trainer.Name,
+            Age = trainer.Age,
+            Birthdate = trainer.Birthdate,
+            CreatedAt = trainer.CreatedAt,
+            Medals = trainer.Medals.Select(m => m.ToDocument()).ToList()
+        };
+    }
+
+    private static Models.Medal ToModel(this Medal request)
+    {
+        return new Models.Medal
+        {
+            Region = request.Region,
+            Type = (Models.MedalType)(int)request.Type
+        };
+    }
+
+    private static MedalDocument ToDocument(this Models.Medal medal)
+    {
+        return new MedalDocument
+        {
+            Region = medal.Region,
+            Type = (MedalTypeMongo)(int)medal.Type
         };
     }
 
@@ -50,5 +92,3 @@ public static class TrainerMapper
         };
     }
 }
-
-
